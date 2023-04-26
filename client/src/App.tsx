2e6daee310c14eba422901623cc381222eff7d2d
ieatn <ieatnubcake@gmail.com>
2023-04-25 18:46:59 -0700

    axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,39 @@
 import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+// npm i axios
+import axios from 'axios';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  // useEffect(() => {
-  //   axios.get('http://localhost:5000/')
-  //     .then(res => {
-  //       setMessage(res.data);
-  //     })
-  //     .catch(err => {
-  //       console.log(err);
-  //     });
-  // }, []);
 
   type Battery = {
     id: number;
     name: string;
     price: number;
   };
-
   const [batteries, setBatteries] = useState<Battery[]>([]);
+
+  // AXIOS
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/battery')
-      .then(res => res.json())
-      .then(data => setBatteries(data))
-      .catch(error => console.log(error))
+    axios.get('http://localhost:5000/battery')
+      .then(res => {
+        setBatteries(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, []);
-  // const [batteries, setBatteries] = useState<Battery[]>([]);
+
+  // REST API
   // useEffect(() => {
   //   fetch('http://127.0.0.1:5000/battery')
-  //     .then(response => response.json())
+  //     .then(res => res.json())
   //     .then(data => setBatteries(data))
-  //     .catch(error => console.log(error));
+  //     .catch(error => console.log(error))
   // }, []);
+
+ 
 
   return (
     <div>
-      <p>{message}</p>
       {batteries.map((battery: Battery) => (
         <div key={battery.id}>
           <h2>{battery.name}</h2>
